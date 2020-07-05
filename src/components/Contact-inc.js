9aec5cec08ace021c182c668de32b15c2feee61b
twilightscapes <ihatespam@gmail.com>
2020-07-05 02:12:48 -0500

    Update Contact-inc.js

--- src/components/Contact-inc.js
+++ src/components/Contact-inc.js
@@ -66,7 +66,7 @@ export default class Contact extends React.Component {
               
 
               <form
-                name="Hytron Contact"
+                name="contact-inc"
                 method="post"
                 action="/contact/success/"
                 data-netlify="true"
@@ -87,7 +87,7 @@ Contact Us
                     Don’t fill this out:{' '}
                     <input name="bot-field" onChange={this.handleChange} />
                   </label>
-                  <label htmlFor="form-name">upload</label><input type="hidden" name="form-name" value="Hytron Contact" />
+                  <label htmlFor="form-name">upload</label><input type="hidden" name="form-name" value="contact-inc" />
                 </div>
 
                 <div className="form">
